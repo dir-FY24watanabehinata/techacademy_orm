38c5ce49e61000f30789292ee1935e90cd8afbb3
dir-FY24watanabehinata <hinata.watanabe.g848@dirbato.co.jp>
2024-04-17 15:51:23 +0900

    Lesson11

---
+++ techacademy_orm/src/action/JpaTest.java
@@ -0,0 +1,23 @@
+package action;
+
+import javax.persistence.EntityManager;
+
+import entity.City;
+import util.DBUtil;
+
+public class JpaTest {
+
+    public static void main(String[] args) {
+        //EntityManagerのオブジェクトを生成
+        EntityManager em=DBUtil.createEntityManager();
+        
+        //１件取得して名前を表示する
+        City city=em.find(City.class,1);
+        System.out.println(city.getName());
+        
+        //EntityManagerの利用を終了する
+        em.close();
+
+    }
+
+}
